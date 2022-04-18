2fb11fb30788ce32049f813906c92d75d7e77bfb
ShahnewajNomi <shahnewajnomi@gmail.com>
2022-04-18 22:13:16 +0600

    Q1 added

--- src/component/Services/Services.js
+++ src/component/Services/Services.js
@@ -8,7 +8,11 @@ const Services = () => {
         // Showing serevice
         <div>
             <div>
-                <h1 className="text-center text-primary my-3">My Services</h1>
+                <h1 className="text-center text-primary my-3">My Answers</h1>
+            </div>
+            <div className='text-center'>
+                <h2 className='text-danger'>Q1.what is the difference between authentication and authorization?</h2>
+                <h5 className='text-center p-5'>Authentication is the act of confirming a person's identity, at the same time as authentication is the process of confirming a user's get proper of access to to precise apps, files, and data. The scenario is similar to that of an airline figuring out which passengers can board.</h5>
             </div>
             
         </div>
